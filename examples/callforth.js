93bf3a54321d850ef0e8834344a692cd39feb7e2
The Narrator <starduu@Justin-C-Moores-MacBook-Pro.local>
2013-10-12 02:01:43 -0500

    Support multiple import methods and add Forth.pass.

--- examples/callforth.js
+++ examples/callforth.js
@@ -1,17 +1,18 @@
 var Forth = require("../forth"),
 	forth = Forth.go ( function start (callforth) {
 
-		setTimeout(function () {
-			callforth.report(null, Date.now());
-			callforth.report(null, Date.now());
-			callforth.report(null, Date.now());
+		var result = [1, 2, 3];
 
-			if (Date.now() % 3) {
-				callforth ( null, [1, 2, 3] );
-			} else {
-				callforth ( new Error("bad"), null );
-			}
-		}, 0);
+		callforth.report(null, Date.now());
+		callforth.report(null, Date.now());
+		callforth.report(null, Date.now());
+
+		if (Date.now() % 3) {
+			callforth ( null, result );
+			result.push(4); // NOTE: callforth is delayed
+		} else {
+			callforth ( new Error("bad"), null );
+		}
 
 	} );
 
@@ -19,9 +20,9 @@ forth.good ( function success (callforth, data) {
 	console.log(["success", data]);
 } ).bad( function die (err, callforth) {
 	console.log(["die", err]);
-	callforth.apply(null, arguments);
-}).last( function end (callforth, err, data) {
-	console.log(["end", err, data]);
+	callforth(err, [5, 6, 7, 8]);
+}).last( function after (callforth, err, data) {
+	console.log(["after", err, data]);
 });
 
 forth.bad ( function fail (err, callforth) {
@@ -31,3 +32,15 @@ forth.bad ( function fail (err, callforth) {
 forth.status( function progress (callforth, warn, info) {
 	console.log(["progress", warn, info]);
 } );
+
+forth = forth.pass ();
+
+forth.bad ( function either (err, callforth) {
+	console.log(["either", err]);
+});
+
+forth.good ( function or (callforth, data) {
+	console.log(["or", data]);
+}).bad ( function not (err, callback) {
+	console.log(["not", err]);
+});
